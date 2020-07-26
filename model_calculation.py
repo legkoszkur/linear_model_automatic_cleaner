6986577a7bc6c41992ceec83ca08584015978cb2
legkoszkur <65813842+legkoszkur@users.noreply.github.com>
2020-07-26 18:57:49 +0200

    Update model_calculation.py

--- model_calculation.py
+++ model_calculation.py
@@ -3,7 +3,7 @@ from model_specification import y_endo
 from data_input import the_data
 import statsmodels.api as sm
 
-new_model = sm.OLS(endog = the_data[y_endo],exog =  the_data[x_egzo]) # tutaj popełniałem taki błąd że zamiast OLS pisałem ols
+new_model = sm.OLS(endog = the_data[y_endo],exog =  the_data[x_egzo]) 
 results = new_model.fit()
 print(results.summary())
 print("Under p-values:")
